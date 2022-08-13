032a9ea170020d89c282e0847a91af25fa5f1dd1
Mujib <muhdiqmal1997@gmail.com>
2022-08-14 00:47:15 +0800

    update bug fix

--- composeexample/web_app/urls.py
+++ composeexample/web_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('states',views.states,name='states'),
+path('/states',views.states,name='states'),
 path('',views.home,name='home'),
-path('delete_city',views.delete_city,name='delete_city'),
-path('edit_city_form',views.edit_city_form,name='edit_city_form'),
-path('get_weather',views.get_weather,name='get_weather'),
-path('cityList',views.get_city_list,name='cityList'),
-path('add_city',views.create_city,name='add_city'),
-path('update_city',views.update_city,name='update_city'),
-path('city_form',views.city_form,name='city_form'),
-path('remove_city',views.remove_city,name='remove_city'),
-path('get_weather/by_city_name',views.by_city_name,name='by_city_name')
+path('/delete_city',views.delete_city,name='delete_city'),
+path('/edit_city_form',views.edit_city_form,name='edit_city_form'),
+path('/get_weather',views.get_weather,name='get_weather'),
+path('/cityList',views.get_city_list,name='cityList'),
+path('/add_city',views.create_city,name='add_city'),
+path('/update_city',views.update_city,name='update_city'),
+path('/city_form',views.city_form,name='city_form'),
+path('/remove_city',views.remove_city,name='remove_city'),
+path('/get_weather/by_city_name',views.by_city_name,name='by_city_name')
 ]
